12bad22c5fb44ec490a1466a7fba83180f667137
Teuschl Nikolas <niklas.teuschl@hotmail.com>
2023-03-30 15:14:28 +0200

    Tried to fix permission error in testcase -> Github Actions

--- test/pyodb/schema/shard_schema_test.py
+++ test/pyodb/schema/shard_schema_test.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from unittest import TestCase
-from shutil import rmtree
 import os
 
 from src.pyodb.schema._shard_schema import ShardSchema
@@ -9,18 +8,21 @@ from test.test_models.complex_models import ComplexBasic
 
 
 class ShardSchemaTest(TestCase):
-    def test_add_type(self):
-        base_path = Path(".pyodb")
-        rmtree(base_path.as_posix() + "/")
-        base_path.mkdir(755)
+    def setUp(self) -> None:
+        self.base_path = Path(".pyodb")
+        for file in os.listdir(self.base_path.as_posix()):
+            os.remove(self.base_path / file)
+        return super().setUp()
 
-        schema = ShardSchema(base_path, 2)
+
+    def test_add_type(self):
+        schema = ShardSchema(self.base_path, 2)
         schema.add_type(ComplexBasic)
 
         self.assertIn(ComplexBasic, schema._tables)
         self.assertIn(PrimitiveBasic, schema._tables)
         self.assertIn(PrimitiveContainer, schema._tables)
 
-        db_list = os.listdir(base_path.as_posix())
+        db_list = os.listdir(self.base_path.as_posix())
         for _, table in schema._tables.items():
             self.assertIn(table.name + ".db", db_list)
